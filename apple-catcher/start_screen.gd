1949556ce1a76dcb0b9d80967cc2497136ec699e
sibbed <1468709@sd61learn.ca>
2025-12-09 14:00:24 -0800

    music

--- apple-catcher/start_screen.gd
+++ apple-catcher/start_screen.gd
@@ -2,6 +2,18 @@ extends Node2D
 
 func _ready():
 	$"start button".pressed.connect(_on_start_pressed)
+	$TutorialPage.visible = false
 
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://better_apples.tscn")
+
+
+func _on_tutorial_button_button_down() -> void:
+	$TutorialPage.visible = true
+	
+	
+	
+
+
+func _on_mainmenu_button_down() -> void:
+	$TutorialPage.visible = false
