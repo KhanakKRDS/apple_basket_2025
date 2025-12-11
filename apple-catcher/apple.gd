ea4ffdb00c61eed9c1aac5df8a98bd5a7b2fa25f
Tavery-06 <tavery19.lavender@gmail.com>
2025-12-11 15:03:10 -0800

    change scoe to global-score

--- apple-catcher/apple.gd
+++ apple-catcher/apple.gd
@@ -1,29 +1,19 @@
 extends CharacterBody2D
 
-var score = 0
 var score_label: Label
 
 func _ready():
-	velocity = Vector2(0, 300) # x and y position
+	velocity = Vector2(0, 300)
 	score_label = get_node("/root/BetterApples/score")
 
 func _physics_process(delta: float) -> void:
-	if position.y >680:
-		position.x = randi_range(0, 1100)
-		position.y = 0
-		
-
 	move_and_slide()
 
 	# Missed apple
 	if position.y > 680:
 		reset_apple()
-		# Decrease score if missed
-		score = max(score - 1, 0)  # prevent negative score
-		score_label.text =("Score: ") + str(score)
-		var score = int(score_label.text)
-		
-		
+		Global.score = max(Global.score - 1, 0)
+		score_label.text = "Score: " + str(Global.score)
 
 func reset_apple():
 	position.x = randi_range(0, 1100)
--- apple-catcher/basket.gd
+++ apple-catcher/basket.gd
@@ -1,29 +1,24 @@
 extends Area2D
 
-var score = 0
-
 var score_label: Label
 
 func _ready():
 	score_label = get_node("/root/BetterApples/score")
+	score_label.text = "Score: " + str(Global.score)
 
-	
 func _process(delta):
 	if Input.is_key_pressed(KEY_LEFT):
-		position.x -=10
-		
-	elif  Input.is_key_pressed(KEY_RIGHT):
-		position.x +=10
-	if Input.is_key_pressed(KEY_D):
+		position.x -= 10
+	elif Input.is_key_pressed(KEY_RIGHT):
 		position.x += 10
+
 	if Input.is_key_pressed(KEY_A):
 		position.x -= 10
-
+	if Input.is_key_pressed(KEY_D):
+		position.x += 10
 
 func _on_body_entered(body: Node) -> void:
-	# Only reset apple if method exists
 	if body.has_method("reset_apple"):
-		var score = int(score_label.text)
-		score += 1
-		score_label.text = ("Score: ") + str(score)
+		Global.score += 1                        
+		score_label.text = "Score: " + str(Global.score)
 		body.reset_apple()
